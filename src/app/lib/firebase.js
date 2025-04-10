df72dda0bbabeed0cf9c81abcaf6112ef95c1c5b
unknown <rgcreation598@gmail.com>
2025-04-10 16:37:21 +0530

    new changes

--- src/app/lib/firebase.js
+++ src/app/lib/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
 import { getAnalytics } from "firebase/analytics"
-import { getFirestore } from "firebase/firestore"
+import { doc, getFirestore, setDoc } from "firebase/firestore"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -9,23 +9,22 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyCn0l763gouCq9QbnmdGsk8GlaIeSgDxMk",
-	authDomain: "missionjobadminpanel-d55bb.firebaseapp.com",
-	projectId: "missionjobadminpanel-d55bb",
-	storageBucket: "missionjobadminpanel-d55bb.firebasestorage.app",
-	messagingSenderId: "573621079107",
-	appId: "1:573621079107:web:525fa444fc6297bdb158f6",
-	measurementId: "G-HZK3GL56XK",
+	apiKey: "AIzaSyDhI-xmTn7pNDYzISS1WbnfGGAfDLoU2PM",
+	authDomain: "missonjobportal.firebaseapp.com",
+	projectId: "missonjobportal",
+	storageBucket: "missonjobportal.firebasestorage.app",
+	messagingSenderId: "631191077926",
+	appId: "1:631191077926:web:0242f41393e65d4ad712e7",
+	measurementId: "G-229FSW9WDT",
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
-// Initialize Firebase
+export const analytics = getAnalytics(app)
 
 // Initialize Firestore (works on both client and server)
 const db = getFirestore(app)
 export const auth = getAuth(app)
 export const provider = new GoogleAuthProvider()
 
-export { db, analytics }
+export { db }
