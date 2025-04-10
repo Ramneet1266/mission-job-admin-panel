aa630a64d63c3b1b5179b4b926387644e98480e6
unknown <rgcreation598@gmail.com>
2025-04-10 15:04:17 +0530

    changes in firebase configuration

--- src/app/lib/firebase.js
+++ src/app/lib/firebase.js
@@ -1,36 +1,31 @@
-// src/app/lib/firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app"
+import { getAnalytics } from "firebase/analytics"
 import { getFirestore } from "firebase/firestore"
-import { getAnalytics, isSupported } from "firebase/analytics"
 import { getAuth, GoogleAuthProvider } from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your Firebase configuration
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyARXWKblkHfRWYNLY3EKri0eGwXGRaSCA0",
-	authDomain: "postjob-b643c.firebaseapp.com",
-	projectId: "postjob-b643c",
-	storageBucket: "postjob-b643c.firebasestorage.app",
-	messagingSenderId: "678507770777",
-	appId: "1:678507770777:web:a5a07ee76c27d18d53d908",
-	measurementId: "G-X6B4L33WV5",
+	apiKey: "AIzaSyCn0l763gouCq9QbnmdGsk8GlaIeSgDxMk",
+	authDomain: "missionjobadminpanel-d55bb.firebaseapp.com",
+	projectId: "missionjobadminpanel-d55bb",
+	storageBucket: "missionjobadminpanel-d55bb.firebasestorage.app",
+	messagingSenderId: "573621079107",
+	appId: "1:573621079107:web:525fa444fc6297bdb158f6",
+	measurementId: "G-HZK3GL56XK",
 }
 
-// Initialize Firebase app
+// Initialize Firebase
 const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
+// Initialize Firebase
 
 // Initialize Firestore (works on both client and server)
 const db = getFirestore(app)
 export const auth = getAuth(app)
 export const provider = new GoogleAuthProvider()
-
-// Initialize Analytics only on the client side
-let analytics = null
-if (typeof window !== "undefined") {
-	isSupported().then((supported) => {
-		if (supported) {
-			analytics = getAnalytics(app)
-		}
-	})
-}
 
 export { db, analytics }
